0568835c3f86af792972fad89461fcbccb731a73
Natural <18321019+Afdul2021@users.noreply.github.com>
2023-07-02 17:44:52 +0100

    Update Syx.h
    FindPattern Functions Added.

--- Syx/Syx.h
+++ Syx/Syx.h
@@ -1,59 +1,79 @@
 #include <stdexcept>
-#include <vector>
 #include <Windows.h>
 
 class Syx
 {
 public:
     /**
-     * Constructs a Syx instance.
+     * Compares data byte by byte using a mask.
      *
-     * @param baseAddress The base address to start the nested pointer dereferencing.
+     * @param pData Pointer to the data to compare.
+     * @param bMask Pointer to the comparison mask.
+     * @param szMask Pointer to the mask string.
+     * @return True if the data matches the mask, false otherwise.
      */
-    Syx(uintptr_t baseAddress) : baseAddress_(baseAddress) {}
+    static bool DataCompare(const BYTE* pData, const BYTE* bMask, const char* szMask)
+    {
+        for (; *szMask; ++szMask, ++pData, ++bMask)
+        {
+            if (*szMask == 'x' && *pData != *bMask)
+            {
+                return false;
+            }
+        }
+        return (*szMask) == NULL;
+    }
+
+    /**
+     * Searches for a pattern in a specified memory region.
+     *
+     * @param dwAddress Starting address of the memory region.
+     * @param dwLen Length of the memory region to search.
+     * @param bMask Pointer to the pattern mask.
+     * @param szMask Pointer to the mask string.
+     * @return The address where the pattern was found, or 0 if not found.
+     */
+    static DWORD64 FindPattern(DWORD64 dwAddress, DWORD dwLen, BYTE* bMask, char* szMask)
+    {
+        for (DWORD i = 0; i < dwLen; i++)
+        {
+            if (DataCompare((BYTE*)(dwAddress + i), bMask, szMask))
+            {
+                return (DWORD64)(dwAddress + i);
+            }
+        }
+        return 0;
+    }
 
     /**
      * Writes a value to a memory location specified by a nested pointer.
-     * This function performs nested pointer dereferencing to calculate the final memory address.
-     * It checks the validity of each pointer level and throws an exception if an invalid pointer is encountered.
      *
+     * @param baseAddress The base address to start the nested pointer dereferencing.
      * @param offsets A vector of offsets that define the path to the desired memory location.
      * @param valueHere The value to be written to the memory location.
-     *
      * @throw std::runtime_error if an invalid pointer is encountered during the dereferencing process.
      */
     template <typename T>
-    void WritePTR(const std::vector<uintptr_t>& offsets, T valueHere)
+    static void WritePTR(uintptr_t baseAddress, const std::vector<uintptr_t>& offsets, T valueHere)
     {
-        uintptr_t* currentPtr = (uintptr_t*)baseAddress_;
+        uintptr_t* currentPtr = (uintptr_t*)baseAddress;
 
-        // Perform nested pointer dereferencing
         try {
             for (size_t i = 0; i < offsets.size(); i++) {
-                // Check if the current pointer is null or invalid
                 if (!currentPtr) {
                     throw std::runtime_error("Invalid pointer encountered.");
                 }
-
-                // Update the pointer to the next level
                 currentPtr = (uintptr_t*)(*currentPtr + offsets[i]);
             }
 
-            // Check if the final pointer is valid for writing
             if (!currentPtr) {
                 throw std::runtime_error("Invalid pointer encountered.");
             }
 
-            // Assign the desired value to the memory location
             *reinterpret_cast<T*>(currentPtr) = valueHere;
         }
         catch (const std::exception& e) {
-            // Handle the error or return an appropriate value
-            // Example: log an error or throw the exception to the caller
-            // ...
             printf("%s", e.what());
-
-            // In this example, we throw the exception to the caller
             throw;
         }
     }
@@ -64,11 +84,9 @@ public:
      * @param moduleName The name of the module.
      * @return The base address of the module if found, or nullptr if the module is not loaded.
      */
-    uintptr_t GetModuleHandleEx(const char* moduleName)
+    static uintptr_t GetModuleHandleEx(const char* moduleName)
     {
         return reinterpret_cast<uintptr_t>(GetModuleHandleA(moduleName));
     }
-
-private:
-    uintptr_t baseAddress_;
 };
+
